4e398775c425639b90ee12fb2eaf86ffeca146ed
kurosaki <kurosaki.kana811@gmail.com>
2022-11-06 22:46:32 +0900

    解答を追加

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,12 @@
 class BonusDrink
-  def self.total_count_for(amount)
-    0
+  def total_count_for(amount)
+    drink_number = amount
+    while amount > 2
+      drink_number += amount/3
+      amount -= (amount/3)*2
+    end
+    return drink_number
   end
 end
+ex1 = BonusDrink.new
+puts "100 : #{ex1.total_count_for(100)}"
